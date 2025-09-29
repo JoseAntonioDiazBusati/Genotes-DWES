c0877318da72a58d58b1ef24d19c28cd0255112b
JoseAntonio <jdiabus511@g.educaand.es>
2025-09-29 21:23:45 +0200

    ejercicio f2

--- src/main/java/com/example/geonotesteaching/Link.java
+++ src/main/java/com/example/geonotesteaching/Link.java
@@ -4,5 +4,18 @@ public record Link(
         String url,
         String label) implements Attachment {
 
+    public Link {
+        if (url == null || url.isBlank()) {
+            throw new IllegalArgumentException("URL REQUERIDA!!!");
+        }
+
+        if (label != null && label.isBlank()) {
+            label = null;
+        }
+    }
+
+    public String effectiveLabel() {
+        return (label == null || label.isBlank()) ? url : label;
+    }
 
 }
