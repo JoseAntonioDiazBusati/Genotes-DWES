2cac2c152e5be76c296f60efb526b33e4904409c
Agsergio04 <agsergio@iesrafaelalberti.es>
2025-10-04 19:41:46 +0200

    Actualizando Errores

--- src/main/java/com/example/geonotesteaching/Timeline.java
+++ src/main/java/com/example/geonotesteaching/Timeline.java
@@ -12,28 +12,36 @@ final class Timeline {
     public Note getNote(long id) { return notes.get(id); }
     public Map<Long, Note> getNotes() { return notes; }
 
-    public final class Render extends AbstractExporter implements Exporter {
-        @Override public String export() {
-            var notesList = notes.values().stream()
+    ppublic final class Render extends AbstractExporter implements Exporter {
+        @Override public String export() {var notesList = notes.values().stream()
+                // Un 'text block' es una cadena de texto multilinea que no necesita
+                // concatenación ni caracteres de escape para las comillas.
                 .map(note -> """
-                        {
-                          "id": %d,
-                          "title": "%s",
-                          "content": "%s",
-                          "location": { "lat": %f, "lon": %f },
-                          "createdAt": "%s"
-                        }
-                        """.formatted(
-                            note.id(), note.title().replace("\",","///"), note.content().replace("\",","///"),
-                            note.location().lat(), note.location().lon(),
-                            note.createdAt()))
+                            {
+                              "id": %d,
+                              "title": "%s",
+                              "content": "%s",
+                              "location": {
+                                "lat": %f,
+                                "lon": %f },
+                              "createdAt": "%s"
+                            }
+                            """.formatted(
+                        note.id(),
+                        note.title(),
+                        note.content(),
+                        note.location().lat(),
+                        note.location().lon(),
+                        note.createdAt()))
                 .sorted(Comparator.reverseOrder())
                 .collect(Collectors.joining(",\n"));
+
             return """
                     { "notes": [ %s ] }
                     """.formatted(notesList);
         }
     }
+    
     public java.util.List<Note> latest(int n) {
         return getNotes()
                 .values().stream()
